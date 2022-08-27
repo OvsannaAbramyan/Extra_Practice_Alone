41071a8af1d37a1294a6b34a705f85c237963901
ovsanna <ovsanna@lionsinserance.com>
2022-08-27 15:11:31 -0700

    Medical String made

---
+++ src/Medical.java
@@ -0,0 +1,11 @@
+public class Medical {
+    public static void main(String[] args) {
+
+
+        String mc = "Medical";
+        String mmc= "Medicare";
+
+        System.out.println(mc);
+    }
+}
+// created some strings
